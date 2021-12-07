1bde9bdd10265cbd8f24b2e4e32f8ec80fe2449c
menes39 <menes39@hotmail.com>
2021-12-07 17:58:22 +0300

    commit

---
+++ script.js
@@ -0,0 +1,50 @@
+//console.log("External");
+
+//let selam = "Hello";
+
+//console.log(selam)
+
+//selam = "Merhaba"
+
+//var deger = "Helloooooooo";
+//console.log(deger)
+//var deger = "Merhaba"
+//console.log(deger)
+// function fonn(){
+//     var deger = "text"
+// }
+
+// fonn();
+// console.log(deger)
+
+// const text = "Javascript";
+
+// text = "Helllo";
+// console.log(text)
+
+var a;
+console.log("type", typeof(a));
+var b;
+console.log("type", typeof(b));
+let id = Symbol()
+
+//reference
+
+var lesson = {
+    name:'Web',
+    number:1
+}
+
+console.log("Object", lesson.name)
+
+
+
+//Array
+
+var dizi = [1, 2, 3];
+console.log("dizi", typeof(dizi));
+
+console.log("item", dizi[1]);
+
+dizi[dizi.length] =4;
+console.log("item", dizi[3])
---
+++ script1.js
@@ -0,0 +1,9 @@
+let dogum = prompt("dogum yılını girin");
+var sayi = 2021-dogum;
+
+if (sayi<18){
+    console.log("Yersin Küçüksün")}
+else if (sayi>65){
+    console.log("Yersin yaşlısın")}
+else{
+console.log("Sokaga cıkabilirsin. uslu dur gene de")}
